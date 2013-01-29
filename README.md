c49a007d9c1dc70c980d3b1f743ba9d7d0746ac0
Teemu Pöntelin <tehapo@gmail.com>
2013-01-29 21:05:45 +0200

    First version using Clara.

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ This tutorial teaches you some of the basic concepts in Vaadin Framework. It is 
 a fast read for learning how to get started - not an example on how application should be
 designed.
 
+The fork you are currently looking at is modified to work as a demonstration of defining 
+the user interface in a declarative fashion. To achieve this we are using the 
+[Clara](https://vaadin.com/addon/clara) add-on.
+
 
 Running the example
 -------------------
--- src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
@@ -1,25 +1,24 @@
 package com.vaadin.tutorial.addressbook;
 
+import org.vaadin.teemu.clara.Clara;
+import org.vaadin.teemu.clara.binder.annotation.UiDataSource;
+import org.vaadin.teemu.clara.binder.annotation.UiField;
+import org.vaadin.teemu.clara.binder.annotation.UiHandler;
+
 import com.vaadin.data.Container.Filter;
 import com.vaadin.data.Item;
-import com.vaadin.data.Property;
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.fieldgroup.FieldGroup;
 import com.vaadin.data.util.IndexedContainer;
 import com.vaadin.event.FieldEvents.TextChangeEvent;
-import com.vaadin.event.FieldEvents.TextChangeListener;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.AbstractTextField.TextChangeEventMode;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.FormLayout;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
 /* 
  * UI class is the starting point for your app. You may deploy it with VaadinServlet
@@ -27,255 +26,198 @@ import com.vaadin.ui.VerticalLayout;
  * app a web page showing your UI is automatically generated. Or you may choose to 
  * embed your UI to an existing web page. 
  */
+@SuppressWarnings({ "serial", "unchecked" })
 public class AddressbookUI extends UI {
 
-	/* User interface components are stored in session. */
-	private Table contactList = new Table();
-	private TextField searchField = new TextField();
-	private Button addNewContactButton = new Button("New");
-	private Button removeContactButton = new Button("Remove this contact");
-	private FormLayout editorLayout = new FormLayout();
-	private FieldGroup editorFields = new FieldGroup();
+    /* User interface components are stored in session. */
+    @UiField("contactList")
+    private Table contactList;
 
-	private static final String FNAME = "First Name";
-	private static final String LNAME = "Last Name";
-	private static final String COMPANY = "Company";
-	private static final String[] fieldNames = new String[] { FNAME, LNAME,
-			COMPANY, "Mobile Phone", "Work Phone", "Home Phone", "Work Email",
-			"Home Email", "Street", "City", "Zip", "State", "Country" };
+    @UiField("searchField")
+    private TextField searchField;
 
-	/*
-	 * Any component can be bound to an external data source. This example uses
-	 * just a dummy in-memory list, but there are many more practical
-	 * implementations.
-	 */
-	IndexedContainer contactContainer = createDummyDatasource();
+    @UiField("removeContactButton")
+    private Button removeContactButton;
 
-	/*
-	 * After UI class is created, init() is executed. You should build and wire
-	 * up your user interface here.
-	 */
-	protected void init(VaadinRequest request) {
-		initLayout();
-		initContactList();
-		initEditor();
-		initSearch();
-		initAddRemoveButtons();
-	}
+    @UiField("editorLayout")
+    private FormLayout editorLayout;
 
-	/*
-	 * In this example layouts are programmed in Java. You may choose use a
-	 * visual editor, CSS or HTML templates for layout instead.
-	 */
-	private void initLayout() {
+    private FieldGroup editorFields = new FieldGroup();
 
-		/* Root of the user interface component tree is set */
-		HorizontalSplitPanel splitPanel = new HorizontalSplitPanel();
-		setContent(splitPanel);
+    private static final String FNAME = "First Name";
+    private static final String LNAME = "Last Name";
+    private static final String COMPANY = "Company";
+    private static final String[] fieldNames = new String[] { FNAME, LNAME,
+            COMPANY, "Mobile Phone", "Work Phone", "Home Phone", "Work Email",
+            "Home Email", "Street", "City", "Zip", "State", "Country" };
 
-		/* Build the component tree */
-		VerticalLayout leftLayout = new VerticalLayout();
-		splitPanel.addComponent(leftLayout);
-		splitPanel.addComponent(editorLayout);
-		leftLayout.addComponent(contactList);
-		HorizontalLayout bottomLeftLayout = new HorizontalLayout();
-		leftLayout.addComponent(bottomLeftLayout);
-		bottomLeftLayout.addComponent(searchField);
-		bottomLeftLayout.addComponent(addNewContactButton);
+    /*
+     * Any component can be bound to an external data source. This example uses
+     * just a dummy in-memory list, but there are many more practical
+     * implementations.
+     */
+    IndexedContainer contactContainer;
 
-		/* Set the contents in the left of the split panel to use all the space */
-		leftLayout.setSizeFull();
+    /*
+     * After UI class is created, init() is executed. You should build and wire
+     * up your user interface here.
+     */
+    protected void init(VaadinRequest request) {
+        /*
+         * Load the content layout from the XML resource file and bind to this
+         * object via annotations (@UiField, @UiDataSource, @UiHandler).
+         */
+        setContent(Clara.create("AddressbookUI.xml", this));
+        contactContainer = (IndexedContainer) contactList
+                .getContainerDataSource();
 
-		/*
-		 * On the left side, expand the size of the contactList so that it uses
-		 * all the space left after from bottomLeftLayout
-		 */
-		leftLayout.setExpandRatio(contactList, 1);
-		contactList.setSizeFull();
+        initContactList();
+        initEditor();
+        initSearch();
+    }
 
-		/*
-		 * In the bottomLeftLayout, searchField takes all the width there is
-		 * after adding addNewContactButton. The height of the layout is defined
-		 * by the tallest component.
-		 */
-		bottomLeftLayout.setWidth("100%");
-		searchField.setWidth("100%");
-		bottomLeftLayout.setExpandRatio(searchField, 1);
+    private void initEditor() {
+        /* User interface can be created dynamically to reflect underlying data. */
+        int index = 0;
+        for (String fieldName : fieldNames) {
+            TextField field = new TextField(fieldName);
+            editorLayout.addComponent(field, index++);
+            field.setWidth("100%");
 
-		/* Put a little margin around the fields in the right side editor */
-		editorLayout.setMargin(true);
-		editorLayout.setVisible(false);
-	}
+            /*
+             * We use a FieldGroup to connect multiple components to a data
+             * source at once.
+             */
+            editorFields.bind(field, fieldName);
+        }
 
-	private void initEditor() {
+        /*
+         * Data can be buffered in the user interface. When doing so, commit()
+         * writes the changes to the data source. Here we choose to write the
+         * changes automatically without calling commit().
+         */
+        editorFields.setBuffered(false);
+    }
 
-		/* User interface can be created dynamically to reflect underlying data. */
-		for (String fieldName : fieldNames) {
-			TextField field = new TextField(fieldName);
-			editorLayout.addComponent(field);
-			field.setWidth("100%");
+    private void initSearch() {
+        /*
+         * Granularity for sending events over the wire can be controlled. By
+         * default simple changes like writing a text in TextField are sent to
+         * server with the next Ajax call. You can set your component to be
+         * immediate to send the changes to server immediately after focus
+         * leaves the field. Here we choose to send the text over the wire as
+         * soon as user stops writing for a moment.
+         */
+        // TODO Setting the TextChangeEventMode not currently supported by Clara
+        searchField.setTextChangeEventMode(TextChangeEventMode.LAZY);
+    }
 
-			/*
-			 * We use a FieldGroup to connect multiple components to a data
-			 * source at once.
-			 */
-			editorFields.bind(field, fieldName);
-		}
-		editorLayout.addComponent(removeContactButton);
+    @UiHandler("searchField")
+    public void doSearch(final TextChangeEvent event) {
+        /* Reset the filter for the contactContainer. */
+        contactContainer.removeAllContainerFilters();
+        contactContainer.addContainerFilter(new ContactFilter(event.getText()));
+    }
 
-		/*
-		 * Data can be buffered in the user interface. When doing so, commit()
-		 * writes the changes to the data source. Here we choose to write the
-		 * changes automatically without calling commit().
-		 */
-		editorFields.setBuffered(false);
-	}
+    @UiHandler("removeContactButton")
+    public void removeSelectedContact(ClickEvent event) {
+        Object contactId = contactList.getValue();
+        contactList.removeItem(contactId);
+    }
 
-	private void initSearch() {
+    @UiHandler("newContactButton")
+    public void addNewContact(ClickEvent event) {
 
-		/*
-		 * We want to show a subtle prompt in the search field. We could also
-		 * set a caption that would be shown above the field or description to
-		 * be shown in a tooltip.
-		 */
-		searchField.setInputPrompt("Search contacts");
+        /*
+         * Rows in the Container data model are called Item. Here we add a new
+         * row in the beginning of the list.
+         */
+        contactContainer.removeAllContainerFilters();
+        Object contactId = contactContainer.addItemAt(0);
 
-		/*
-		 * Granularity for sending events over the wire can be controlled. By
-		 * default simple changes like writing a text in TextField are sent to
-		 * server with the next Ajax call. You can set your component to be
-		 * immediate to send the changes to server immediately after focus
-		 * leaves the field. Here we choose to send the text over the wire as
-		 * soon as user stops writing for a moment.
-		 */
-		searchField.setTextChangeEventMode(TextChangeEventMode.LAZY);
+        /*
+         * Each Item has a set of Properties that hold values. Here we set a
+         * couple of those.
+         */
+        contactList.getContainerProperty(contactId, FNAME).setValue("New");
+        contactList.getContainerProperty(contactId, LNAME).setValue("Contact");
 
-		/*
-		 * When the event happens, we handle it in the anonymous inner class.
-		 * You may choose to use separate controllers (in MVC) or presenters (in
-		 * MVP) instead. In the end, the preferred application architecture is
-		 * up to you.
-		 */
-		searchField.addTextChangeListener(new TextChangeListener() {
-			public void textChange(final TextChangeEvent event) {
+        /* Lets choose the newly created contact to edit it. */
+        contactList.select(contactId);
+    }
 
-				/* Reset the filter for the contactContainer. */
-				contactContainer.removeAllContainerFilters();
-				contactContainer.addContainerFilter(new ContactFilter(event
-						.getText()));
-			}
-		});
-	}
+    private void initContactList() {
+        contactList.setVisibleColumns(new String[] { FNAME, LNAME, COMPANY });
+    }
 
-	/*
-	 * A custom filter for searching names and companies in the
-	 * contactContainer.
-	 */
-	private class ContactFilter implements Filter {
-		private String needle;
+    @UiHandler("contactList")
+    public void contactSelected(ValueChangeEvent event) {
+        Object contactId = contactList.getValue();
 
-		public ContactFilter(String needle) {
-			this.needle = needle.toLowerCase();
-		}
+        /*
+         * When a contact is selected from the list, we want to show that in our
+         * editor on the right. This is nicely done by the FieldGroup that binds
+         * all the fields to the corresponding Properties in our contact at
+         * once.
+         */
+        if (contactId != null) {
+            editorFields.setItemDataSource(contactList.getItem(contactId));
+        }
 
-		public boolean passesFilter(Object itemId, Item item) {
-			String haystack = ("" + item.getItemProperty(FNAME).getValue()
-					+ item.getItemProperty(LNAME).getValue() + item
-					.getItemProperty(COMPANY).getValue()).toLowerCase();
-			return haystack.contains(needle);
-		}
+        editorLayout.setVisible(contactId != null);
+    }
 
-		public boolean appliesToProperty(Object id) {
-			return true;
-		}
-	}
+    /*
+     * Generate some in-memory example data to play with. In a real application
+     * we could be using SQLContainer, JPAContainer or some other to persist the
+     * data.
+     */
+    @UiDataSource("contactList")
+    public IndexedContainer createDummyDatasource() {
+        IndexedContainer ic = new IndexedContainer();
 
-	private void initAddRemoveButtons() {
-		addNewContactButton.addClickListener(new ClickListener() {
-			public void buttonClick(ClickEvent event) {
+        for (String p : fieldNames) {
+            ic.addContainerProperty(p, String.class, "");
+        }
 
-				/*
-				 * Rows in the Container data model are called Item. Here we add
-				 * a new row in the beginning of the list.
-				 */
-				contactContainer.removeAllContainerFilters();
-				Object contactId = contactContainer.addItemAt(0);
+        /* Create dummy data by randomly combining first and last names */
+        String[] fnames = { "Peter", "Alice", "Joshua", "Mike", "Olivia",
+                "Nina", "Alex", "Rita", "Dan", "Umberto", "Henrik", "Rene",
+                "Lisa", "Marge" };
+        String[] lnames = { "Smith", "Gordon", "Simpson", "Brown", "Clavel",
+                "Simons", "Verne", "Scott", "Allison", "Gates", "Rowling",
+                "Barks", "Ross", "Schneider", "Tate" };
+        for (int i = 0; i < 1000; i++) {
+            Object id = ic.addItem();
+            ic.getContainerProperty(id, FNAME).setValue(
+                    fnames[(int) (fnames.length * Math.random())]);
+            ic.getContainerProperty(id, LNAME).setValue(
+                    lnames[(int) (lnames.length * Math.random())]);
+        }
 
-				/*
-				 * Each Item has a set of Properties that hold values. Here we
-				 * set a couple of those.
-				 */
-				contactList.getContainerProperty(contactId, FNAME).setValue(
-						"New");
-				contactList.getContainerProperty(contactId, LNAME).setValue(
-						"Contact");
+        return ic;
+    }
 
-				/* Lets choose the newly created contact to edit it. */
-				contactList.select(contactId);
-			}
-		});
+    /*
+     * A custom filter for searching names and companies in the
+     * contactContainer.
+     */
+    private static class ContactFilter implements Filter {
+        private String needle;
 
-		removeContactButton.addClickListener(new ClickListener() {
-			public void buttonClick(ClickEvent event) {
-				Object contactId = contactList.getValue();
-				contactList.removeItem(contactId);
-			}
-		});
-	}
+        public ContactFilter(String needle) {
+            this.needle = needle.toLowerCase();
+        }
 
-	private void initContactList() {
-		contactList.setContainerDataSource(contactContainer);
-		contactList.setVisibleColumns(new String[] { FNAME, LNAME, COMPANY });
-		contactList.setSelectable(true);
-		contactList.setImmediate(true);
+        public boolean passesFilter(Object itemId, Item item) {
+            String haystack = ("" + item.getItemProperty(FNAME).getValue()
+                    + item.getItemProperty(LNAME).getValue() + item
+                    .getItemProperty(COMPANY).getValue()).toLowerCase();
+            return haystack.contains(needle);
+        }
 
-		contactList.addValueChangeListener(new Property.ValueChangeListener() {
-			public void valueChange(ValueChangeEvent event) {
-				Object contactId = contactList.getValue();
-
-				/*
-				 * When a contact is selected from the list, we want to show
-				 * that in our editor on the right. This is nicely done by the
-				 * FieldGroup that binds all the fields to the corresponding
-				 * Properties in our contact at once.
-				 */
-				if (contactId != null)
-					editorFields.setItemDataSource(contactList
-							.getItem(contactId));
-				
-				editorLayout.setVisible(contactId != null);
-			}
-		});
-	}
-
-	/*
-	 * Generate some in-memory example data to play with. In a real application
-	 * we could be using SQLContainer, JPAContainer or some other to persist the
-	 * data.
-	 */
-	private static IndexedContainer createDummyDatasource() {
-		IndexedContainer ic = new IndexedContainer();
-
-		for (String p : fieldNames) {
-			ic.addContainerProperty(p, String.class, "");
-		}
-
-		/* Create dummy data by randomly combining first and last names */
-		String[] fnames = { "Peter", "Alice", "Joshua", "Mike", "Olivia",
-				"Nina", "Alex", "Rita", "Dan", "Umberto", "Henrik", "Rene",
-				"Lisa", "Marge" };
-		String[] lnames = { "Smith", "Gordon", "Simpson", "Brown", "Clavel",
-				"Simons", "Verne", "Scott", "Allison", "Gates", "Rowling",
-				"Barks", "Ross", "Schneider", "Tate" };
-		for (int i = 0; i < 1000; i++) {
-			Object id = ic.addItem();
-			ic.getContainerProperty(id, FNAME).setValue(
-					fnames[(int) (fnames.length * Math.random())]);
-			ic.getContainerProperty(id, LNAME).setValue(
-					lnames[(int) (lnames.length * Math.random())]);
-		}
-
-		return ic;
-	}
-
+        public boolean appliesToProperty(Object id) {
+            return true;
+        }
+    }
 }
